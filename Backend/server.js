fdc80176a73d08fc485e7ee70528b9ac2714872a
Pranay Talware <pranaytalware7@gmail.com>
2026-05-12 20:21:07 +0530

    authenticationDone

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/auth", authRoutes);
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("JanSeva Backend Running");
